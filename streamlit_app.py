89e55cf7130a87b12ab67324629823dd2826644c
Rohit Kumar Gupta <kumar.rohitgupta@gmail.com>
2023-05-17 18:03:08 +1000

    Update streamlit_app.py
    removed unnecessary JSON output on display

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
 
 # use pandas funstion json_normalize to flatten out the json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
